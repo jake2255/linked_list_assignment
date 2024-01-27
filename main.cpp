6dbf1a070e558363e1fe8124bb9b83cb116cb48e
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-26 23:26:19 -0600

    Update main.cpp
    Added the outputs for user to enter movie info

--- main.cpp
+++ main.cpp
@@ -1,27 +1,41 @@
 #include "linkedlist.h"
-#include <iostream>
-
-using namespace std;
 
 int main(){
 
     Linkedlist list;
     int option = 0;
+    string title, id, genre;
+    int year;
+    float rating;
+
+    cout <<"****MOVIE DATABASE****"<< endl;
     do{
-        cout << "Enter linked list elements [1], display list [2], delete node [3], or quit [4]: ";
+        cout << "Add movie[1], delete movie [2], display movie list [3], or quit [4]: ";
         cin >> option;
 
         if(option == 1){
-            int newVal = 0;
-            cout << "Enter value to add to linked list: ";
-            cin >> newVal;
-            list.InsertNode(newVal);
+            cout << "Movie title: ";
+            cin.ignore();
+            getline(cin, title);
+            cout << "Movie ID: ";
+            getline(cin, id);
+            cout << "Movie genre: ";
+            getline(cin, genre);
+            cout << "Movie release year: ";
+            cin >> year;
+            cout << "Movie rating [1-10]: ";
+            cin >> rating;
+            list.InsertNode(title, id, genre, year, rating);
         }
         else if(option == 2){
-            list.DisplayList();
+            string movieDelete;
+            cout << "Title of movie to delete: ";
+            cin.ignore();
+            getline(cin, movieDelete);
+            list.DeleteNode(movieDelete);
         }
         else if(option == 3){
-            list.DeleteNode();
+            list.DisplayList();
         }
         
     }while(option != 4);
