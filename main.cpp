6a6fac86a7fd2c7a9f516c896203725e7a0fa05e
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-27 21:46:48 -0600

    Update main.cpp
    Fixed the deletenode function. Completed the reverselist, findmidnode, searchnode functions. Added new function displaysinglenode.

--- main.cpp
+++ main.cpp
@@ -1,57 +1,182 @@
 #include "linkedlist.h"
 
-int main(){
+//constructor
+Linkedlist::Linkedlist(){
+    head = nullptr;
+    tail = nullptr;
+}
 
-    Linkedlist list;
-    int option = 0;
-    string title, id, genre;
-    int year;
-    float rating;
+//destructor
+Linkedlist::~Linkedlist(){
+    Node* temp = head;
+    while(temp){
+        Node* nextNode = temp->next;
+        delete temp;
+        temp = nextNode;
+    }
+}
 
-    cout <<"****MOVIE DATABASE****"<< endl;
-    do{
-        cout << "Add movie[1], delete movie [2], display movie list [3], find middle movie [4], search movie [5], reverse list [6] or quit [0]: ";
-        cin >> option;
+//insert new node to end of linked list
+void Linkedlist::InsertNode(string title, string id, string genre, int year, float rating){
+    Node* newNode = new Node;
+    newNode->mTitle = title;
+    newNode->mId = id;
+    newNode->mGenre = genre;
+    newNode->mYear = year;
+    newNode->mRating = rating;
 
-        if(option == 1){
-            cout << "Movie title: ";
-            cin.ignore();
-            getline(cin, title);
-            cout << "Movie ID: ";
-            getline(cin, id);
-            cout << "Movie genre: ";
-            getline(cin, genre);
-            cout << "Movie release year: ";
-            cin >> year;
-            cout << "Movie rating [1-10]: ";
-            cin >> rating;
-            list.InsertNode(title, id, genre, year, rating);
-        }
-        else if(option == 2){
-            string movieDelete;
-            cout << "Title of movie to delete: ";
-            cin.ignore();
-            getline(cin, movieDelete);
-            list.DeleteNode(movieDelete);
-        }
-        else if(option == 3){
-            list.DisplayList();
-        }
-        else if(option == 4){
-            cout << "Movie in middle of list.";
-            list.FindMidNode();
-        }
-        else if(option == 5){
-            string movieToFind;
-            cout << "Enter the title or ID of movie: ";
-            cin >> movieToFind;
-            list.SearchNode(movieToFind);
-        }
-        else if(option == 6){
-            list.ReverseList();
-            list.DisplayList();
-        }
-    }while(option != 0);
+    if(head == nullptr){
+        head = newNode;
+        tail = newNode;
+    }
+    else{
+        tail->next = newNode;
+        tail = newNode;
+    }
+}
 
-    return 0;
+//delete node at the head of list
+void Linkedlist::DeleteNode(string movieDelete){
+    if(head == nullptr){
+        cout << "List empty." << endl;
+        return;
+    }
+    
+    Node* current = head;
+    Node* previous = nullptr;
+    bool movieExist = false;
+
+    while(current && !movieExist){
+        if(current->mTitle == movieDelete){
+            movieExist = true;
+        }
+        else{
+            previous = current;
+            current = current->next;
+        }
+    }
+
+    if(!movieExist){
+        cout << "Movie doesnt exist." << endl;
+    }
+    else if(current == head){
+        if(head->next == nullptr){
+            head = nullptr;
+            tail = nullptr;
+            current = nullptr;
+        }
+        else{
+            head = head->next;
+        }
+    }
+    else if(current == tail){
+        previous->next = nullptr;
+        tail = previous;
+    }
+    else{
+        previous->next = current->next;
+    }
+    //free(current);
+    //free(previous);
+}
+
+//display all elements in linked list
+void Linkedlist::DisplayList(){
+    if(head == nullptr){
+        cout << "List empty." << endl;
+        return;
+    }
+
+    Node* temp = head;
+    int counter = 1;
+
+    cout << "Movie List: " << endl;
+    while(temp){
+        cout << "Movie #" << counter << ": " << temp->mTitle << endl;
+        temp = temp->next;
+        counter++;
+    }
+}
+
+//reverses the linked list
+void Linkedlist::ReverseList(){
+    if(head == nullptr){
+        cout << "List empty." << endl;
+        return;
+    }   
+
+    Node* current = head;
+    Node* previous = nullptr;
+    Node* next = nullptr;
+
+    while(current != NULL){
+        next = current->next;
+        current->next = previous;
+        previous = current;
+        current = next;
+    }
+    head = previous;
+}
+
+//finds the middle element in the linked list
+void Linkedlist::FindMidNode(){
+    if(head == nullptr){
+        cout << "List empty." << endl;
+        return;
+    }
+
+    Node* temp = head;
+    int counter = 1;
+    while(temp){
+        temp = temp->next;
+        counter++;
+    }
+    
+    int midNode = counter / 2;
+
+    DisplaySingleNode(midNode);
+}
+
+//finds and displays a selected node
+void Linkedlist::SearchNode(string movieSearch){
+    if(head == nullptr){
+        cout << "List empty." << endl;
+        return;
+    }
+
+    Node* temp = head;
+    bool found = false;
+    int counter = 1;
+
+    while(temp && !found){
+        if(temp->mTitle == movieSearch || temp->mId == movieSearch){
+            found = true;
+        }
+        temp = temp->next;
+        counter++;
+    }
+
+    if(found){
+        DisplaySingleNode(counter-1);
+    }
+    else{
+        cout << "Movie not found." << endl;
+    }
+}
+
+//displays all the info from a selected node
+void Linkedlist::DisplaySingleNode(int location){
+    Node* temp = head;
+    int counter = 1;
+    while(location != counter){
+        temp = temp->next;
+        counter++;
+    }
+
+    cout << "Movie #" << counter << " in list:" << endl;
+    cout << "Title: " << temp->mTitle << endl;
+    cout << "ID: " << temp->mId << endl;
+    cout << "Genre: " << temp->mGenre << endl;
+    cout << "Release Year: " << temp->mYear << endl;
+    cout << "Rating: " << temp->mRating << endl;
 }
