ca8a55c5686258822130f5b4a0442699587c35cc
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-28 20:25:50 -0600

    Update main.cpp
    updated the layout the user sees

--- main.cpp
+++ main.cpp
@@ -1,182 +1,68 @@
 #include "linkedlist.h"
 
-//constructor
-Linkedlist::Linkedlist(){
-    head = nullptr;
-    tail = nullptr;
-}
+int main(){
 
-//destructor
-Linkedlist::~Linkedlist(){
-    Node* temp = head;
-    while(temp){
-        Node* nextNode = temp->next;
-        delete temp;
-        temp = nextNode;
-    }
-}
+    Linkedlist list;    //linked list object
+    int option = 0; //menu selection
+    string title, id, genre; //user info for movie
+    int year;
+    float rating;
 
-//insert new node to end of linked list
-void Linkedlist::InsertNode(string title, string id, string genre, int year, float rating){
-    Node* newNode = new Node;
-    newNode->mTitle = title;
-    newNode->mId = id;
-    newNode->mGenre = genre;
-    newNode->mYear = year;
-    newNode->mRating = rating;
+    cout <<"****MOVIE DATABASE****"<< endl;
+    do{
+        cout << BLUE_TEXT << "Add movie [1]" << endl;
+        cout << "Delete movie [2]" << endl;
+        cout << "Display movie list [3]" << endl;
+        cout << "Find middle movie [4]" << endl;
+        cout << "Search movie [5]" << endl;
+        cout << "Reverse list [6]" << endl;
+        cout << "Quit program [0]" << WHITE_TEXT << endl;
+        cin >> option;
+        system("clear");
 
-    if(head == nullptr){
-        head = newNode;
-        tail = newNode;
-    }
-    else{
-        tail->next = newNode;
-        tail = newNode;
-    }
-}
-
-//delete node at the head of list
-void Linkedlist::DeleteNode(string movieDelete){
-    if(head == nullptr){
-        cout << "List empty." << endl;
-        return;
-    }
-    
-    Node* current = head;
-    Node* previous = nullptr;
-    bool movieExist = false;
-
-    while(current && !movieExist){
-        if(current->mTitle == movieDelete){
-            movieExist = true;
+        //options to modify the movie database
+        if(option == 1){
+            cout << "Movie title: ";
+            cin.ignore();
+            getline(cin, title);
+            cout << "Movie ID: ";
+            getline(cin, id);
+            cout << "Movie genre: ";
+            getline(cin, genre);
+            cout << "Movie release year: ";
+            cin >> year;
+            cout << "Movie rating [1-10]: ";
+            cin >> rating;
+            list.InsertNode(title, id, genre, year, rating);
+            system("clear");
         }
-        else{
-            previous = current;
-            current = current->next;
+        else if(option == 2){
+            string movieDelete;
+            cout << "Title of movie to delete: ";
+            cin.ignore();
+            getline(cin, movieDelete);
+            list.DeleteNode(movieDelete);
         }
-    }
-
-    if(!movieExist){
-        cout << "Movie doesnt exist." << endl;
-    }
-    else if(current == head){
-        if(head->next == nullptr){
-            head = nullptr;
-            tail = nullptr;
-            current = nullptr;
+        else if(option == 3){
+            list.DisplayList();
         }
-        else{
-            head = head->next;
+        else if(option == 4){
+            list.FindMidNode();
         }
-    }
-    else if(current == tail){
-        previous->next = nullptr;
-        tail = previous;
-    }
-    else{
-        previous->next = current->next;
-    }
-    //free(current);
-    //free(previous);
-}
-
-//display all elements in linked list
-void Linkedlist::DisplayList(){
-    if(head == nullptr){
-        cout << "List empty." << endl;
-        return;
-    }
-
-    Node* temp = head;
-    int counter = 1;
-
-    cout << "Movie List: " << endl;
-    while(temp){
-        cout << "Movie #" << counter << ": " << temp->mTitle << endl;
-        temp = temp->next;
-        counter++;
-    }
-}
-
-//reverses the linked list
-void Linkedlist::ReverseList(){
-    if(head == nullptr){
-        cout << "List empty." << endl;
-        return;
-    }   
-
-    Node* current = head;
-    Node* previous = nullptr;
-    Node* next = nullptr;
-
-    while(current != NULL){
-        next = current->next;
-        current->next = previous;
-        previous = current;
-        current = next;
-    }
-    head = previous;
-}
-
-//finds the middle element in the linked list
-void Linkedlist::FindMidNode(){
-    if(head == nullptr){
-        cout << "List empty." << endl;
-        return;
-    }
-
-    Node* temp = head;
-    int counter = 1;
-    while(temp){
-        temp = temp->next;
-        counter++;
-    }
-    
-    int midNode = counter / 2;
-
-    DisplaySingleNode(midNode);
-}
-
-//finds and displays a selected node
-void Linkedlist::SearchNode(string movieSearch){
-    if(head == nullptr){
-        cout << "List empty." << endl;
-        return;
-    }
-
-    Node* temp = head;
-    bool found = false;
-    int counter = 1;
-
-    while(temp && !found){
-        if(temp->mTitle == movieSearch || temp->mId == movieSearch){
-            found = true;
+        else if(option == 5){
+            string movieToFind;
+            cout << "Enter the title or ID of movie: ";
+            cin >> movieToFind;
+            list.SearchNode(movieToFind);
         }
-        temp = temp->next;
-        counter++;
-    }
+        else if(option == 6){
+            list.ReverseList();
+            list.DisplayList();
+        }
 
-    if(found){
-        DisplaySingleNode(counter-1);
-    }
-    else{
-        cout << "Movie not found." << endl;
-    }
-}
+    }while(option != 0);
 
-//displays all the info from a selected node
-void Linkedlist::DisplaySingleNode(int location){
-    Node* temp = head;
-    int counter = 1;
-    while(location != counter){
-        temp = temp->next;
-        counter++;
-    }
+    cout << "have a nice day";
 
-    cout << "Movie #" << counter << " in list:" << endl;
-    cout << "Title: " << temp->mTitle << endl;
-    cout << "ID: " << temp->mId << endl;
-    cout << "Genre: " << temp->mGenre << endl;
-    cout << "Release Year: " << temp->mYear << endl;
-    cout << "Rating: " << temp->mRating << endl;
+    return 0;
 }
