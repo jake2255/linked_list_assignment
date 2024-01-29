1cac160ac714a55334d6a7821194fe3e9a99a975
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-01-28 20:27:35 -0600

    Update linkedlist.h
    Added necessary tags for colored text

--- linkedlist.h
+++ linkedlist.h
@@ -3,9 +3,14 @@
 
 #include <iostream>
 #include <string>
+#define BLUE_TEXT "\x1b[36m"
+#define WHITE_TEXT "\x1b[0m"
+#define GREEN_TEXT "\x1b[32m"
+#define RED_TEXT "\x1b[31m"
 
 using namespace std;
 
+//struct that stores all the elements in each node
 struct Node{
     Node* next;
     string mTitle;
@@ -15,6 +20,7 @@ struct Node{
     float mRating;
 };
 
+//class that implements the nodes into a linked list
 class Linkedlist{
     private:
         Node* head;
