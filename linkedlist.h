fcfee8d43f49117c6b3828f593412393d99e1eda
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-02-25 20:29:41 -0600

    Update linkedlist.h
    Changed class to work as student database, removed unnecessary functions

--- linkedlist.h
+++ linkedlist.h
@@ -3,21 +3,20 @@
 
 #include <iostream>
 #include <string>
+
 #define BLUE_TEXT "\x1b[36m"
 #define WHITE_TEXT "\x1b[0m"
 #define GREEN_TEXT "\x1b[32m"
 #define RED_TEXT "\x1b[31m"
 
-using namespace std;
-
 //struct that stores all the elements in each node
 struct Node{
     Node* next;
-    string mTitle;
-    string mId;
-    string mGenre;
-    int mYear;
-    float mRating;
+    std::string s_name;
+    std::string s_address;
+    std::string s_dob;
+    std::string s_id;
+    Node(std::string n, std::string a, std::string d, std::string i): s_name(n), s_address(a), s_dob(d), s_id(i){}
 };
 
 //class that implements the nodes into a linked list
@@ -28,13 +27,11 @@ class Linkedlist{
     public:
         Linkedlist();
         ~Linkedlist();
-        void InsertNode(string title, string id, string genre, int year, float rating);
-        void DeleteNode(string movieDelete);
-        void DisplayList();
-        void ReverseList();
-        void FindMidNode();
-        void SearchNode(string movieSearch);
-        void DisplaySingleNode(int location);
+        void InsertNode(std::string, std::string, std::string, std::string);
+        void DeleteNode(std::string);
+        void SearchNode(std::string);
+        void UpdateNode();
+        void DisplayList();    
 };
 
 #endif
