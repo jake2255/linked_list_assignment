c932078a537439362f32670c906cbc8556765ec6
Jacob Ulloa <157094404+jake2255@users.noreply.github.com>
2024-03-10 10:39:43 -0500

    completed UpdateNode function, modified code for new student variables added

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -17,9 +17,9 @@ Linkedlist::~Linkedlist(){
 }
 
 //insert new node to end of linked list
-void Linkedlist::InsertNode(std::string name, std::string address, std::string dob, std::string id){
+void Linkedlist::InsertNode(std::string name, std::string street, std::string city, std::string state, std::string zip, std::string dob, std::string id){
     //initializes new node with user info
-    Node* newNode = new Node(name, address, dob, id);
+    Node* newNode = new Node(name, street, city, state, zip, dob, id);
     //if list is empty set head/tail to new node, else append new node to tail
     if(head == nullptr){
         head = newNode;
@@ -94,10 +94,14 @@ void Linkedlist::SearchNode(std::string studentToFind){
             counter++;
         }
     }
+
     if(studentExist){
         std::cout << GREEN_TEXT << "Student #" << counter << " in list:" << WHITE_TEXT << std::endl;
         std::cout << "Name: " << temp->s_name << std::endl;
-        std::cout << "Address: " << temp->s_address << std::endl;
+        std::cout << "Street: " << temp->s_street << std::endl;
+        std::cout << "City: " << temp->s_city << std::endl;
+        std::cout << "State: " << temp->s_state << std::endl;
+        std::cout << "Zipcode: " << temp->s_zipcode << std::endl;
         std::cout << "Date of Birth: " << temp->s_dob << std::endl;
         std::cout << "ID: " << temp->s_id << std::endl;
     }
@@ -105,8 +109,36 @@ void Linkedlist::SearchNode(std::string studentToFind){
         std::cout << RED_TEXT << "Student not found." << WHITE_TEXT << std::endl;
 }
 
-void Linkedlist::UpdateNode(){
+//finds and updates a selected node
+void Linkedlist::UpdateNode(std::string studentToUpdate, std::string name, std::string street, std::string city, std::string state, std::string zip, std::string dob){
+    if(head == nullptr){
+        std::cout << RED_TEXT << "List empty." << WHITE_TEXT << std::endl;
+        return;
+    } 
 
+    Node* temp = head;
+    bool studentExist = false;
+    int counter = 1;
+
+    //iterates through list until student name/id is found
+    while(temp && !studentExist){
+        if(temp->s_name == studentToUpdate || temp->s_id == studentToUpdate)
+            studentExist = true;
+        else{
+            temp = temp->next;
+            counter++;
+        }
+    }
+    if(studentExist){
+       temp->s_name = name;
+       temp->s_street = street;
+       temp->s_city = city;
+       temp->s_state = state;
+       temp->s_zipcode = zip;
+       temp->s_dob = dob;
+    }
+    else
+        std::cout << RED_TEXT << "Student not found." << WHITE_TEXT << std::endl;
 }
 
 //display all elements in linked list
@@ -117,13 +149,11 @@ void Linkedlist::DisplayList(){
     }
 
     Node* temp = head;
-    int counter = 1;
 
     //iterates through list displaying student names
     std::cout << "Student List: " << std::endl;
     while(temp){
-        std::cout << GREEN_TEXT << "Student #" << counter << WHITE_TEXT << ": " << temp->s_name << std::endl;
+        std::cout << GREEN_TEXT << "Student, ID->" << WHITE_TEXT << temp->s_name << ", " << temp->s_id << std::endl;
         temp = temp->next;
-        counter++;
     }
 }
